31538680b37ac3532812e2e7cb9aa76006f71dd6
xeler <xeler.arsk@gmail.com>
2022-03-19 14:54:18 +0300

    First Edit

--- climat_controll/Files.ino
+++ climat_controll/Files.ino
@@ -1,17 +1,17 @@
 void save(){
-  EEPROM.update(0, 0);
-  EEPROM.put(1, setting);
+  EEPROM.update(0, 0); // Записываем в первую ячейку 0, потому что по умолчанию там 255
+  EEPROM.put(1, setting); // Записываем настройки
   Serial.println("Saved");
 }
 void load(){
-  if(EEPROM[0] == 255) return;
-  EEPROM.get(1, setting);
+  if(EEPROM[0] == 255) return; // Проверяем первую ячейку памяти, если она равна 255, то нечего не делаем
+  EEPROM.get(1, setting); // Если не равна 255, то читаем настройки
   
   updatePID();
   
   Serial.println("Loaded");
 }
 void resetSave(){
-  EEPROM.update(0, 255);
+  EEPROM.update(0, 255); // Записываем в первую ячейку 255, обнуляя ячейку
   Serial.println("Save reset");
 }
