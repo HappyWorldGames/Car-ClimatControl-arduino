e8e4c4ec8d28617618823f490399a24d64e9f735
HappyWorldGames <xeler.arsk@gmail.com>
2022-07-18 13:15:48 +0300

    add ServiceAssist.h

---
+++ arduino_climat_controll_program/ServiceAssist.h
@@ -0,0 +1,22 @@
+
+/*
+ * Commands:
+ * 	1 - управление заслонкой
+ * 	2 -
+ */
+class ServiceAssist {
+	private:
+		
+	public:
+		ServiceAssist() {
+			
+		}
+
+		void cmd(int[] cmds) {
+			switch(cmds[0]) {
+				case 0:
+					
+					break;
+			}
+		}
+}
